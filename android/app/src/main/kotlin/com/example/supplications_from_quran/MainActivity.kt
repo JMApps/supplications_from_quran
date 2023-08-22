340e4f25076fd50dbb9adaaece46826e782a6fca
dev_muslim <jmappsmuslim@gmail.com>
2023-08-22 11:04:51 +0300

    Clear all project

---
+++ android/app/src/main/kotlin/com/example/supplications_from_quran/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.supplications_from_quran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:supplications_from_quran/presentation/pages/root_page.dart';
+
+void main() {
+  runApp(
+    const RootPage(),
+  );
+}
