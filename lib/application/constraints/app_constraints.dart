f6754695cfd9697f6044532cbaa1c6ff8d62b129
dev_muslim <jmappsmuslim@gmail.com>
2023-08-25 14:16:18 +0300

    Added translation text for all languages

--- lib/application/constraints/app_constraints.dart
+++ lib/application/constraints/app_constraints.dart
@@ -1,3 +1,18 @@
 class AppConstraints {
   static const keyMainAppSettings = 'key_main_app_settings';
+
+  // Settings
+  static const String keyFontIndex = 'key_sfq_font_index';
+  static const String keyArabicFontIndex = 'key_sfq_arabic_font_index';
+  static const String keyTextAlign = 'key_sfq_text_align';
+  static const String keyArabicTextAlign = 'key_sfq_arabic_text_align';
+  static const String keyTextSize = 'key_sfq_text_size';
+  static const String keyArabicTextSize = 'key_sfq_arabic_text_size';
+  static const String keyArabicLightTextColor = 'key_sfq_arabic_light_text_color';
+  static const String keyArabicDarkTextColor = 'key_sfq_arabic_dark_text_color';
+  static const String keyLightTextColor = 'key_sfq_light_text_color';
+  static const String keyDarkTextColor = 'key_sfq_dark_text_color';
+  static const String keyAdaptiveTheme = 'key_sfq_adaptive_theme';
+  static const String keyDarkTheme = 'key_sfq_dark_theme';
+  static const String keyWakeLock = 'key_sfq_wake_lock';
 }
--- lib/application/strings/app_strings.dart
+++
@@ -1,3 +0,0 @@
-class AppStrings {
-  static const String appName = 'Мольбы из Корана';
-}
