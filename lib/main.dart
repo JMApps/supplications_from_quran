11190034bde1549156daacfdd81c83f20cada485
jmapps <jmappsmuslim@gmail.com>
2024-12-04 23:19:34 +0300

    Small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:supplications_from_quran/application/constraints/app_constraints.dart';
-import 'package:supplications_from_quran/application/state/content_settings_state.dart';
-import 'package:supplications_from_quran/application/state/main_app_state.dart';
-import 'package:supplications_from_quran/data/services/database_helper.dart';
-import 'package:supplications_from_quran/presentation/pages/root_page.dart';
+
+import 'application/constraints/app_constraints.dart';
+import 'application/state/content_settings_state.dart';
+import 'application/state/main_app_state.dart';
+import 'data/services/database_helper.dart';
+import 'presentation/pages/root_page.dart';
 
 void main() async {
-  if (Platform.isAndroid) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        systemNavigationBarColor: Colors.transparent,
-      ),
-    );
-  }
   WidgetsFlutterBinding.ensureInitialized();
+
   DatabaseHelper databaseHelper = DatabaseHelper();
   await databaseHelper.initializeDatabase();
+
   await Hive.initFlutter();
   await Hive.openBox(AppConstraints.keyMainAppSettings);
   await Hive.openBox(AppConstraints.keyFavoriteSupplications);
+
   runApp(
     MultiProvider(
       providers: [
--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -3,45 +3,42 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:supplications_from_quran/application/state/content_settings_state.dart';
-import 'package:supplications_from_quran/application/themes/app_themes.dart';
-import 'package:supplications_from_quran/presentation/pages/main_page.dart';
-import 'package:supplications_from_quran/presentation/widgets/default_scroll_behavior.dart';
+
+import '../../application/state/content_settings_state.dart';
+import '../../application/themes/app_themes.dart';
+import 'main_page.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ContentSettingsState contentSettingsState = Provider.of<ContentSettingsState>(context);
-    return MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-          PointerDeviceKind.stylus,
-          PointerDeviceKind.trackpad,
-          PointerDeviceKind.unknown,
-        },
-      ),
-      builder: (context, child) {
-        return ScrollConfiguration(
-          behavior: DefaultScrollBehavior(),
-          child: child!,
+    return Consumer<ContentSettingsState>(
+      builder: (context, contentSettingsState, _) {
+        return MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
+          debugShowCheckedModeBanner: false,
+          scrollBehavior: const MaterialScrollBehavior().copyWith(
+            dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.touch,
+              PointerDeviceKind.stylus,
+              PointerDeviceKind.trackpad,
+              PointerDeviceKind.unknown,
+            },
+          ),
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+          themeMode: contentSettingsState.getAdaptiveTheme
+              ? ThemeMode.system
+              : contentSettingsState.getDarkTheme
+              ? ThemeMode.dark
+              : ThemeMode.light,
+          home: const MainPage(),
         );
       },
-      theme: AppThemes.lightTheme,
-      darkTheme: AppThemes.darkTheme,
-      themeMode: contentSettingsState.getAdaptiveTheme
-          ? ThemeMode.system
-          : contentSettingsState.getDarkTheme
-          ? ThemeMode.dark
-          : ThemeMode.light,
-      home: const MainPage(),
     );
   }
 }
