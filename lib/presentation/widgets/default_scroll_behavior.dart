8d7204a23fe071ee7da1e5414d8324a3360024b8
jmapps <jmappsmuslim@gmail.com>
2024-12-11 22:47:30 +0300

    Added new strings

--- lib/presentation/widgets/default_scroll_behavior.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class DefaultScrollBehavior extends ScrollBehavior {
-  @override
-  ScrollPhysics getScrollPhysics(BuildContext context) => const BouncingScrollPhysics();
-}
